465acec7009c2cee9c0ce9d6749a63ce2b6d9cc9
yuri <yurimarques626@gmail.com>
2023-12-16 21:28:50 -0300

    get method

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (request, response) =>{
+    response.send('Hello World')
+})
+
 const PORT = 3333
 app.listen(PORT, () => console.log(`ola, estou na porta ${PORT}`))
